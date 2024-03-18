00e696e50695823370feabd2ac684a3013bbe2dd
Vladislav Shevchuk <109815421+vladikshevchuk@users.noreply.github.com>
2024-03-18 08:09:42 +0100

    implemented storage.services and func saveToken

--- weather.js
+++ weather.js
@@ -1,6 +1,20 @@
 #!/usr/bin/env node
-import { getArgs } from "./helpers/args.js";
-import { printHelp } from "./services/log.service.js";
+import { getArgs } from './helpers/args.js';
+import { printHelp, printSuccess, printError } from './services/log.service.js';
+import { saveKeyValue } from './services/storage.service.js';
+
+const saveToken = async (token) => {
+    if (!token.length) {
+        printError('Не передан token');
+        return;
+    }
+    try {
+        await saveKeyValue('token', token);
+        printSuccess('Токен сохранен')
+    } catch (err) {
+        printError(err.message);
+    }
+}
 
 const initCLI = () => {
     const args = getArgs(process.argv);
@@ -12,7 +26,7 @@ const initCLI = () => {
         // Save sity
     };
     if (args.t) {
-        // Save tokken
+        return saveToken(args.t)
     };
     // Show weather
 };
